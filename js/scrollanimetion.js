0680f919ab460aa0bf994fac9003556e3e653ed4
Uchida310 <mitsuya.uchida310@gmail.com>
2021-12-04 17:46:34 +0900

    フェードインJSを導入/ハンバーガーまだ

---
+++ js/scrollanimetion.js
@@ -0,0 +1,11 @@
+var scrollAnimationElm = document.querySelectorAll('.sa');//
+var scrollAnimationFunc = function() {
+  for(var i = 0; i < scrollAnimationElm.length; i++) {
+    var triggerMargin = 300;
+    if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
+      scrollAnimationElm[i].classList.add('show');
+    }   
+  }
+}
+window.addEventListener('load', scrollAnimationFunc);
+window.addEventListener('scroll', scrollAnimationFunc);
