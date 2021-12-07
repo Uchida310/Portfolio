2bad38ba9a8d465d087c14bc3ec00a981a7bc765
Uchida310 <mitsuya.uchida310@gmail.com>
2021-12-07 15:24:23 +0900

    baseページresponsive完成要ゲーム紹介

--- js/scrollanimetion.js
+++ js/scrollanimetion.js
@@ -1,7 +1,7 @@
 var scrollAnimationElm = document.querySelectorAll('.sa');//
 var scrollAnimationFunc = function() {
   for(var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = 300;
+    var triggerMargin = 200;
     if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
       scrollAnimationElm[i].classList.add('show');
     }   
