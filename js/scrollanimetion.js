b53843b71376df956867c76b64b8a8c015126f22
Uchida310 <mitsuya.uchida310@gmail.com>
2021-12-12 17:37:32 +0900

    scrollAnimationのｺｰﾄﾞ解析/fadeinアニメーションの動作を改良

--- js/scrollanimetion.js
+++ js/scrollanimetion.js
@@ -1,11 +1,32 @@
-var scrollAnimationElm = document.querySelectorAll('.sa');//
+var scrollAnimationElm = document.querySelectorAll('.sa');
+const fadein = document.getElementById('fade-anim');
+let cnt;
 var scrollAnimationFunc = function() {
   for(var i = 0; i < scrollAnimationElm.length; i++) {
-    var triggerMargin = 200;
+    var triggerMargin = 300;
+    //innerheight(ﾍﾟｰｼﾞが表示される高さ)>class(sa)の位置+200の時
     if (window.innerHeight > scrollAnimationElm[i].getBoundingClientRect().top + triggerMargin) {
-      scrollAnimationElm[i].classList.add('show');
+      //class(sa)のクラスにshowを追加する
+      scrollAnimationElm[i].classList.add('show');  
+      fadein.classList.add('fadein');
     }   
   }
 }
 window.addEventListener('load', scrollAnimationFunc);
 window.addEventListener('scroll', scrollAnimationFunc);
+
+/*
+要素.getBoundingClientRect()メソッド・・
+要素の位置を、ブラウザ表示領域の左上を基点として、
+そこからの相対値を持つtopなら要素の上端
+*/
+
+/*
+addEventListener()メソッド・・・さまざまのイベント処理を実行できる
+webﾍﾟｰｼﾞが読み込まれたか？
+クリックされたか？
+フォームに何かしらの操作が行われたか？
+キーボードから入力が行われたか？
+構文:対象要素.addEventListener(イベントの種類,メソッド,)
+今回のイベントは読み込みが完了したときloadと画面がスクロールした時
+*/
